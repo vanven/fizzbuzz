425df20c092165242c6dcf7afc5139b9a61ffec6
Vanessa Vengco <vengco@gmail.com>
2013-07-23 16:23:18 -0700

    Add test for first element as 1

--- fizzbuzz_spec.rb
+++ fizzbuzz_spec.rb
@@ -9,5 +9,9 @@ describe FizzBuzz do
  	it "returns an array" do
  		expect(FizzBuzz.new.do_it).to be_instance_of(Array)
  	end
+
+ 	it "first element is 1" do
+ 		expect(subject.do_it[0]).to eql(1)
+ 	end
  end
 end

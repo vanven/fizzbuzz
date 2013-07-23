14d522d69dba2977852e57077aaa3856c610834e
Vanessa Vengco <vengco@gmail.com>
2013-07-23 16:12:54 -0700

    Add test for array

--- fizzbuzz_spec.rb
+++ fizzbuzz_spec.rb
@@ -4,4 +4,10 @@ describe FizzBuzz do
  it "is not nil" do
  	expect(FizzBuzz).not_to be_nil
  end
+
+ describe "#make_fizz_buzz" do
+ 	it "returns an array" do
+ 		expect(FizzBuzz.new.do_it).to be_an_instance_of_array
+ 	end
+ end
 end

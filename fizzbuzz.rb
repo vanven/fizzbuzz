22f16dad78397a3b17a0a9a182a1391619494632
Mariana Lenetis <mlenetis@gmail.com>
2013-07-23 16:04:03 -0700

    Fix typo in test. Nil test passes.

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,2 +1,3 @@
 class FizzBuzz
+
 end
--- fizzbuzz_spec.rb
+++ fizzbuzz_spec.rb
@@ -2,6 +2,6 @@ require './fizzbuzz'
 
 describe FizzBuzz do
  it "is not nil" do
- 	expect(Fizzbuzz).not_to be_nil
+ 	expect(FizzBuzz).not_to be_nil
  end
 end

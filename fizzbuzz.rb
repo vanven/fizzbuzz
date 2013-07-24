292d3d3a1f598ac3e56330e74f289bf1656bdde9
Vanessa Vengco <vengco@gmail.com>
2013-07-23 17:17:50 -0700

    Pass every third element is fizz test

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -3,8 +3,10 @@ class FizzBuzz
     fb_array = Array.new
     for i in (1..100)
       fb_array << i
+      if i%3 == 0
+      	fb_array[i-1] = "fizz"
+      end
     end
-    fb_array[2] = "fizz"
     return fb_array
 
 

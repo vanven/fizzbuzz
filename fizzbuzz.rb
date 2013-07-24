b692475cf39859d42ea90c8cf167c9cb9d8bd856
Vanessa Vengco <vengco@gmail.com>
2013-07-23 17:40:54 -0700

    Pass fifteenth element equal to FizzBuzz test

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -4,10 +4,13 @@ class FizzBuzz
     for i in (1..100)
       fb_array << i
       if i%3 == 0
-      	fb_array[i-1] = "fizz"
+      	fb_array[i-1] = "Fizz"
       end
       if i%5 == 0
-      	fb_array[i-1] = "buzz"
+      	fb_array[i-1] = "Buzz"
+      end
+      if i%3 == 0 && i%5 == 0
+      	fb_array[i-1] = "FizzBuzz"
       end
     end
     return fb_array
